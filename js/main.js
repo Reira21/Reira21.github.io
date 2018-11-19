9ccab988de996f974d473125a6fdd1155253596a
JessicaFelix <jessicateixeira32@gmail.com>
2018-11-19 23:20:37 +0000

    [upd] Safari bug fix
    Doesn't accept variables "let" on javascript

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ window.onscroll=function(e){
 }
       
 function checkVisibilityAndMove(id, percent){
-    let domElem = $("#" + id).get(0);
+    var domElem = $("#" + id).get(0);
 
     if(isScrolledIntoView(domElem)){
         move(domElem, percent);
