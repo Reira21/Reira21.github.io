e20351ca2ee3ea86a3512721705feddbdfd34e0e
JessicaFelix <jessicateixeira32@gmail.com>
2018-12-02 19:57:33 +0000

    [new] Menu web

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
-    
-//    $( "#header" ).load( "./header.html",function(){
-//        console.log("header carregado");
-//    });
-//
-//    
+        
     $( "#about" ).load( "./sobre.html",function(){
         console.log("about carregado");
     });
@@ -22,11 +17,13 @@ $(document).ready(function () {
     });
 
     $( "#contact" ).load( "./formulario.html",function(){
-        console.log("contact carregado");
+        console.log("contact carregado");    
     });
   
     var scene = document.getElementById('js-scene');
     var parallax = new Parallax(scene);
+    
+    setupNavbar();
 });
 
 window.onscroll=function(e){
@@ -80,3 +77,53 @@ function isScrolledIntoView(elem){
 
    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
+
+function setupNavbar (){
+   	var contentSections = $('.cd-section'),
+		navigationItems = $('#cd-vertical-nav a');
+
+	updateNavigation(contentSections, navigationItems);
+	$(window).on('scroll', function(){
+		updateNavigation(contentSections, navigationItems);
+	});
+
+	//smooth scroll to the section
+	navigationItems.on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+    //smooth scroll to second section
+    $('.cd-scroll-down').on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+
+    //open-close navigation on touch devices
+    $('.touch .cd-nav-trigger').on('click', function(){
+    	$('.touch #cd-vertical-nav').toggleClass('open');
+  
+    });
+    //close navigation on touch devices when selectin an elemnt from the list
+    $('.touch #cd-vertical-nav a').on('click', function(){
+    	$('.touch #cd-vertical-nav').removeClass('open');
+    }); 
+}
+
+	function updateNavigation(contentSections, navigationItems) {
+		contentSections.each(function(){
+			$this = $(this);
+			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+				navigationItems.eq(activeSection).addClass('is-selected');
+			}else {
+				navigationItems.eq(activeSection).removeClass('is-selected');
+			}
+		});
+	}
+
+	function smoothScroll(target) {
+        $('body,html').animate(
+        	{'scrollTop':target.offset().top},
+        	600
+        );
+	}
